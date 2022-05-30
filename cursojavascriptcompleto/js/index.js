9491fcb9ce6828b88d0e2be513899b6c8175e74a
andersontecnicoprogramador <68762932+andersontecnicoprogramador@users.noreply.github.com>
2022-05-30 11:56:43 -0300

    Add files via upload

---
+++ cursojavascriptcompleto/js/index.js
@@ -0,0 +1,31 @@
+//alert("ola mundo ")
+//document.getElementById("texto").innerHTML="meu primeiro texto<b>JS</br>!";
+//console.log("oi isso é um console.log");
+//Declarações
+//let a,b,c; a=5;b=6;c=7;
+//sintaxe
+//var a = 1;
+//var b = 5;
+//var c = a + b; //posso varios sinais artimetico
+//console.log(c);
+//Comentarios
+//Essa aula bem curta de comentarios
+/* Vamos entender Variáveis
+Variáveis são "recipientes" onde podemos armazenar informações que 
+podem variar, ou seja, podem ter qualquer tipo de valor .
+
+No Javascript temos 3 palavras- chave para declarar variáveis :
+-> var
+-> let
+-> const
+*/
+
+//Declaração de variaveis 
+var a,b,c;
+
+// atribuição dos valores
+a=2;
+b=3;
+c= a + b;
+
+alert(c);
